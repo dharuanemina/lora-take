d97014e89ad30c8a7afb4bb6bffe97ede3b6a01e
Dharuan Emina <dharuan.emina@compasso.com.br>
2021-06-28 22:47:30 -0300

    Server file renamed

--- challenge/api/src/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const server = require("express")();
-const routes = require("./router")
-
-server.use(routes)
-
-server.listen(process.env.PORT, () => console.log(`Server running at: ${process.env.URL}:${process.env.PORT}`));
-module.exports = server;
