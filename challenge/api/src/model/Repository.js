9b8248ff612205b314e0fbb45c0fa671d9caea98
Dharuan Emina <dharuan.emina@compasso.com.br>
2021-06-29 15:54:19 -0300

    Create method to get last 20 repos

--- challenge/api/src/model/Repository.js
+++ challenge/api/src/model/Repository.js
@@ -2,32 +2,52 @@ const axios = require("axios")
 const errorUtils = require("../utils/ErrorUtils")
 
 module.exports = {
+    // return last repositories by custom quantity
     async getLastRepos(user, lang, quant) {
         let response = {
             status: 200,
             message: "Ok",
-            quant: 0,
             data: {
+                quant: 0,
                 results: []
             }
         }
 
-        try {
-            
-            const getContent = await axios.get(`https://api.github.com/users/${user}/repos?sort=created&direction=asc&per_page=10&page=1`)
+        // create recursive function paginate when request other languages...
+        // todo -> create function query for better requests
 
-            getContent.data.forEach((e) => {
-                if(e.language && e.language.toLowerCase() === lang.toLowerCase()) {
-                    response.data.results.push(e)
+        const repos = await this.getLast20Repo(user, 1)
+
+        if(repos.length > 0) {
+            repos.forEach((repo) => {
+                if(repo.language && repo.language.toLowerCase() == lang.toLowerCase()) {
+                    if(response.data.results.length < quant) {
+                        response.data.results.push(repo)
+                        response.data.quant++
+                    }
                 }
             })
+        }
+        else {
+            response = {
+                ...repos
+            }
+        }
 
-            console.log(response.data.results.length)
+        return response
 
-            return response
+    },
+    // return last fifty repositories paginated
+    async getLast20Repo(user, page) {
+        try {
+
+            let getContent = await axios.get(`https://api.github.com/users/${user}/repos?sort=created&direction=asc&per_page=20&page=${page}`)
+
+            return getContent.data
+
         }
         catch(e) {
-            console.log(e)
+            return errorUtils.errorDataNormalize(e)
         }
-    }
+    },
 }
